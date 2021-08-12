84e49fbf92a93fead1928dfc08542a31c0a4299b
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-08-12 15:41:23 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,58 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+import pandas as pd
+import numpy as np
+
+#This class maps values in a column, numeric or categorical.
+#Importantly, it does not change NaNs, leaving that for the imputer step.
+class MappingTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, mapping_column, mapping_dict:dict):  
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print("Warning: MappingTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MappingTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'MappingTransformer.transform unknown column {self.mapping_column}'
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+    
+
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=True):  
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+ 
+  def fit(self, X, y = None):
+    print("Warning: OHETransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'OHETransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.target_column in X.columns.to_list(), f'OHETransformer.transform unknown column {self.target_column}'
+    X_ = X.copy()
+    X_ = pd.get_dummies(X_, columns=[self.target_column],
+                        dummy_na=self.dummy_na,
+                        drop_first = self.drop_first)
+    print(f'OHETransformer columns: {X_.columns.to_list()}')
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+
+#from week 2
+titanic_transformer_v1 = Pipeline(steps=[
+    ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('class', MappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('ohe', OHETransformer(target_column='Joined')),
+    ], verbose=True)
